edac650e59f76013ed652b358f6b8052ed3ad219
Peter Jones <kephail@me.com>
2019-11-30 15:07:23 +0000

    Congrats component & passing tests

--- src/Congrats.js
+++ src/Congrats.js
@@ -2,8 +2,19 @@ import React from "react";
 
 /**
  * Functional React component for congratulations message.
+ * @param {object} - React props.
  * @returns {JSX.Element}
  */
-export default () => {
-  return <div />;
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
