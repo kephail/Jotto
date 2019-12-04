d7b5209853ea0e93a3ea4bfc402731e8ab1ac8f8
Peter Jones <kephail@me.com>
2019-12-04 14:47:08 +0000

    Continue writing tests for GuessedWords component

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -44,4 +44,30 @@ describe("If there are no words guessed.", () => {
   });
 });
 
-describe("If there are guessed words.", () => {});
+describe("If there are guessed words.", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 },
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+
+  it("Renders without error.", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  it("Renders 'guessed words' section.", () => {
+    const guessedWordNode = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordNode.length).toBe(1);
+  });
+
+  it("Displays the correct number of guessed words.", () => {
+    const guessedWordNodes = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordNodes.length).toBe(guessedWords.length);
+  });
+});
